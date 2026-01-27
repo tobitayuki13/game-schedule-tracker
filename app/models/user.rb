3a69ade8aeeb26804baa7ea7892f614547b39a1c
あなたの名前 <>
2026-01-27 20:10:17 +0900

    ユーザー認証機能の実装(ログインログアウト)

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6 }, if: -> { password.present? }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get "home/index"
+  get    "/login",  to: "sessions#new"
+  post   "/login",  to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # トップページ(仮)
+  root "home#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
