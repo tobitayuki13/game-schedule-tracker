cad4c6b7081630d92272a03bdf7e28c5578c4cc1
あなたの名前 <>
2026-01-30 15:39:05 +0900

    Add Task model and verify Routine-Task associations

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,3 +1,4 @@
 class Routine < ApplicationRecord
   belongs_to :user
+  has_many :tasks, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_27_111709) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_30_062147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_27_111709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_routines_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.bigint "routine_id", null: false
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["routine_id"], name: "index_tasks_on_routine_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +41,5 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_27_111709) do
   end
 
   add_foreign_key "routines", "users"
+  add_foreign_key "tasks", "routines"
 end
